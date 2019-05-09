cbad8b2053226ff3c75f8cec1d1c6e7508688882
DBServer - Gabriel Novakovski Nunes <t.gabriel.nunes@zaffari.com.br>
2019-05-09 14:06:49 -0300

    Alteracões iniciais

--- src/js/app.js
+++ src/js/app.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "type": "polygon",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#303F9F"
         },
         "polygon": {
           "nb_sides": 5
